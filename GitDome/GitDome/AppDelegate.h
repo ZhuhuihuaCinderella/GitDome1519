1c33bd9a7e674f6e9117f8084decdd64727f6c16
zhuhuihua <2369699113@qq.com>
2016-02-26 14:07:59 +0800

    first commit project

---
+++ GitDome/GitDome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDome
+//
+//  Created by Qianfeng on 16/2/26.
+//  Copyright © 2016年 Cinderella Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDome/GitDome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDome
+//
+//  Created by Qianfeng on 16/2/26.
+//  Copyright © 2016年 Cinderella Zhu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
