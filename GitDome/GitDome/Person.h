a6acbb2e832ad64d870370a14c8730cca38ed871
zhuhuihua <2369699113@qq.com>
2016-02-26 14:20:17 +0800

    work1 创建一个person类

---
+++ GitDome/GitDome/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  GitDome
+//
+//  Created by Qianfeng on 16/2/26.
+//  Copyright © 2016年 Cinderella Zhu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ GitDome/GitDome/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  GitDome
+//
+//  Created by Qianfeng on 16/2/26.
+//  Copyright © 2016年 Cinderella Zhu. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
